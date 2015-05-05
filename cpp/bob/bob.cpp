d63d11c97630e783a83942fee8f2be481eaad082
ogre <ogre@localhost.localdomain>
2015-05-04 22:54:24 -0400

    First foray into C++

---
+++ cpp/bob/bob.cpp
@@ -0,0 +1,20 @@
+/* bob.cpp
+	Exercism.IO exercise 1
+	Tom Roderick
+	Began: May 4, 2015
+	Finish: 
+ */
+
+
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+	int arrayLength;
+	string statement;
+	statement = "WATCH OUT";
+	std::cout << statement;
+	return 0;
+}
